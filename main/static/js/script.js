4475768218d57652df021067e8440ab5742e1697
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2020-11-08 21:00:25 +0500

    Add new photos

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -52,21 +52,360 @@ const images = [
   },
 ];
 
+const room1 = [
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/1.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/2.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/3.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/4.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/5.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/6.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/7.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/8.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/9.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/10.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/11.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/12.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/13.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/14.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/15.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/16.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/17.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/18.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-1/19.jpg",
+    w: 0,
+    h: 0,
+  },
+];
+
+const room2 = [
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/1.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/2.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/3.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/4.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/5.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/6.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/7.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/8.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/9.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/10.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/11.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/12.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/13.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/14.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/15.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/16.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/17.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/18.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/19.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/20.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/21.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/22.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/23.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-2/24.jpg",
+    w: 0,
+    h: 0,
+  },
+];
+
+const room3 = [
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/1.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/2.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/3.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/4.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/5.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/6.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/7.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/8.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/9.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/10.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/11.jpg",
+    w: 0,
+    h: 0,
+  },
+  {
+    src: "https://xn----7sbblh9beecscegov.xn--p1ai/static/images/room-3/12.jpg",
+    w: 0,
+    h: 0,
+  },
+];
+
 const options = {
   index: 0,
   closeOnScroll: false,
 };
 
 const galleryContainer = document.querySelectorAll(".pswp")[0];
-const galleryToggle = document.getElementById("run-gallery");
+// const galleryToggle = document.getElementById("run-gallery");
+const galleryToggle1 = document.getElementById("run-gallery-1");
+const galleryToggle2 = document.getElementById("run-gallery-2");
+const galleryToggle3 = document.getElementById("run-gallery-3");
 
-galleryToggle.addEventListener("click", (e) => {
+galleryToggle1.addEventListener("click", (e) => {
   e.preventDefault();
 
   let gallery = new PhotoSwipe(
     galleryContainer,
     PhotoSwipeUI_Default,
-    images,
+    room1,
+    options
+  );
+  gallery.listen("gettingData", function (index, item) {
+    if (item.w < 1 || item.h < 1) {
+      // unknown size
+      var img = new Image();
+      img.onload = function () {
+        item.w = this.width;
+        item.h = this.height;
+        gallery.invalidateCurrItems();
+        gallery.updateSize(true);
+      };
+      img.src = item.src;
+    }
+  });
+
+  gallery.init();
+});
+
+galleryToggle2.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let gallery = new PhotoSwipe(
+    galleryContainer,
+    PhotoSwipeUI_Default,
+    room2,
+    options
+  );
+  gallery.listen("gettingData", function (index, item) {
+    if (item.w < 1 || item.h < 1) {
+      // unknown size
+      var img = new Image();
+      img.onload = function () {
+        item.w = this.width;
+        item.h = this.height;
+        gallery.invalidateCurrItems();
+        gallery.updateSize(true);
+      };
+      img.src = item.src;
+    }
+  });
+
+  gallery.init();
+});
+
+galleryToggle3.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let gallery = new PhotoSwipe(
+    galleryContainer,
+    PhotoSwipeUI_Default,
+    room3,
     options
   );
   gallery.listen("gettingData", function (index, item) {
