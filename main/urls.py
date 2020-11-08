d1821e924234b2b8db86dc4fe356cdbc2ad32517
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2020-11-09 03:10:44 +0500

    Add menu page

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^tour/', tour, name='tour'),
+    url(r'^$', Index, name='index'),
+    url(r'^tour/', Tour, name='tour'),
+    url(r'^menu/', MainMenu, name='main_menu'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from .models import *
 
 
-def index(request):
-    menu = Menu.objects.all()
+def Index(request):
+    return render(request, 'index.html', context={})
+
+def Tour(request):
+    return render(request, 'tour.html', context={})
+
+def MainMenu(request):
+    specimen = MenuSpecimen.objects.get(title='Меню')
 
     context = {
-        'menu': menu,
+        'specimen': specimen,
+        'menu': Menu.objects.filter(specimen=specimen),
     }
 
-    return render(request, 'index.html', context=context)
-
-def tour(request):
-    return render(request, 'tour.html', context={})
+    return render(request, 'menu.html', context=context)
